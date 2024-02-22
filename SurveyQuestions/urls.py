8309c6973ed7819a62a4b1c3d4bfbfc12c70b21d
ColeRun <cwalk@guilford.edu>
2024-02-22 10:19:56 -0500

    TemplatePage set

--- SurveyQuestions/urls.py
+++ SurveyQuestions/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from SurveyQuestions import views
 
 urlpatterns = [
+    #create a path to the index view using index.html
     path('', views.index, name='index'),
-    path('survey/', views.survey, name='survey'),
+   
 ]
--- SurveyQuestions/views.py
+++ SurveyQuestions/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#call survey template one
+def index(request):
+    return render(request, 'home1.html')
