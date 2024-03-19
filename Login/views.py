d073826e8d61a5260b3ee358cb7d04eb40600575
Cole Walk <cwalk@guilford.edu>
2024-03-18 22:08:14 -0400

    playing with views

--- Login/views.py
+++ Login/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+import Models as models
 
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 
-def login(request):
-    return render(request, 'LoginPrompt.html')
+
+
+
+
 
