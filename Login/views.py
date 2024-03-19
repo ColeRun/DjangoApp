27c162a18b713ee74b1f67f8263f883e8e830fcc
Cole Walk <cwalk@guilford.edu>
2024-03-19 10:27:52 -0400

    Linked the two login pages

--- Login/views.py
+++ Login/views.py
@@ -4,6 +4,10 @@ import django.contrib.auth.views
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+import django.contrib.auth.models
+
+
+
 
 def login(request):
  #create logic that if register is clicked it will redirect to register page
@@ -14,15 +18,24 @@ def login(request):
         user = django.contrib.auth.authenticate(request, username=username, password=password)
         if user is not None:
             django.contrib.auth.login(request, user)
-            return redirect('survey')
+            return redirect('/survey')
         else:
-            
-            return render(request, 'LoginPrompt.html')
+            message = 'Username or password is incorrect'
+            #display the message
+            return render(request, 'LoginPrompt.html', {'message': message})
+    else:
+        return render(request, 'LoginPrompt.html')
             
 
-    return render(request, 'LoginPrompt.html')
+   
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = django.contrib.auth.models.User.objects.create_user(username=username, password=password)
+        user.save()
+        return redirect('login')
     return render(request, 'NewLogin.html')
 
 
