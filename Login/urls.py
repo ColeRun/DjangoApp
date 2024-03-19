93bed4e766cda1564e9163c46f89b68e329840ee
Cole Walk <cwalk@guilford.edu>
2024-03-19 09:55:39 -0400

    Created New Login and logic with saving user name and password using django authenticate

--- Login/urls.py
+++ Login/urls.py
@@ -1,8 +1,10 @@
 # Description: This file contains the URL patterns for the Login app
 from django.urls import path
 from Login import views
+import django.contrib.auth.urls
 
 urlpatterns = [
    
     path('', views.login, name='login'),
+    path('register', views.register, name='register'),
 ]
--- Login/views.py
+++ Login/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-import Models as models
+import django.contrib.auth.views
 
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+def login(request):
+ #create logic that if register is clicked it will redirect to register page
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = django.contrib.auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            django.contrib.auth.login(request, user)
+            return redirect('survey')
+        else:
+            
+            return render(request, 'LoginPrompt.html')
+            
+
+    return render(request, 'LoginPrompt.html')
+
+def register(request):
+    return render(request, 'NewLogin.html')
+
+
+
 
 
 
