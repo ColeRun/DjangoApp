698dfb9baa5d9519565ded809a7f2aa143c5a6f3
Cole Walk <cwalk@guilford.edu>
2024-03-24 22:23:58 -0400

    script for saving surveys

--- SurveyQuestions/models.py
+++ SurveyQuestions/models.py
@@ -1,2 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
   
+
+class Survey(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- SurveyQuestions/views.py
+++ SurveyQuestions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Survey, Question, Answer
 
 # Create your views here.
 #call survey template one
@@ -6,4 +7,15 @@ def index(request):
     return render(request, 'home1.html')
 
 def createsurvey(request):
-    return render(request, 'create_survey.html')
+    if request.method == 'POST':
+        survey = Survey(title=request.POST['title'], user=request.user)
+        survey.save()
+        for question_text in request.POST.getlist('questions'):
+            question = Question(text=question_text, survey=survey)
+            question.save()
+            for answer_text in request.POST.getlist('answers'):
+                answer = Answer(text=answer_text, question=question)
+                answer.save()
+        return render(request, 'survey_created.html')
+    else:
+        return render(request, 'create_survey.html')
