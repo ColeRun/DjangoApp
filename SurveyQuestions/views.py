eb9ac84a9831b263adb59e6a0b02bd6c0b089b1f
Cole Walk <cwalk@guilford.edu>
2024-03-26 10:38:28 -0400

    working on survey-creation

--- SurveyQuestions/views.py
+++ SurveyQuestions/views.py
@@ -10,18 +10,26 @@ def createsurvey(request):
     if request.method == 'POST':
         survey = Survey(title=request.POST['title'], user=request.user)
         survey.save()
+        print("survey created")
         for question_text in request.POST.getlist('questions'):
             question = Question(text=question_text, survey=survey)
             question.save()
+            print("question created")
             for answer_text in request.POST.getlist('answers'):
                 answer = Answer(text=answer_text, question=question)
                 answer.save()
-        return render(request, 'survey_created.html')
+                print("answer created")
+        return survey_detail(request, survey.pk)
     else:
         return render(request, 'create_survey.html')
-    
+
 
 def survey_detail(request, pk):
-    survey = Survey.objects.get(pk=pk)
-    questions = Question.objects.filter(survey=survey)
-    return render(request, 'survey_detail.html', {'survey': survey, 'questions': questions})
+    try:
+        survey = Survey.objects.get(pk=pk)
+        questions = Question.objects.filter(survey=survey)
+        return render(request, 'survey_detail.html', {'survey': survey, 'questions': questions})
+    except Survey.DoesNotExist:
+        return render(request, 'survey_not_found.html')
+    
+    #recreate create survey template to be able to take variables "questions", "answers" and "survey" if user clicks edit survey.
