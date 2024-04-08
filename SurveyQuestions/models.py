1f6df29bb04187bc8eb63dcf66778faf7157a7c0
ColeRun <cwalk@guilford.edu>
2024-04-07 23:00:21 -0400

    Update SurveyQuestions models to use more descriptive choices

--- SurveyQuestions/models.py
+++ SurveyQuestions/models.py
@@ -9,8 +9,8 @@ class Survey(models.Model):
 class Question(models.Model):
     TYPE_CHOICES = (
         ('text', 'Text'),
-        ('radio', 'Radio'),
-        ('checkbox', 'Checkbox'),
+        ('radio', 'multiple choice'),
+        ('checkbox', 'All That Apply'),
     )
     text = models.CharField(max_length=200)
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
